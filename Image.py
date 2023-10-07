b9a100353651169011dc572035dcd5bec465b7a2
Gkayal2 <123992158+Yagbe@users.noreply.github.com>
2023-10-07 17:44:24 -0400

    Update Image.py

--- Image.py
+++ Image.py
@@ -1,24 +1,35 @@
+import cv2 as cv
 
-import cv2 as cv 
+def shift_hue(image, target_hue):
+    hsv_image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 
+    hsv_image[:, :, 0] = target_hue
 
-input_image = cv.imread('images/random_image_ (119).jpg')
+    shifted_image = cv.cvtColor(hsv_image, cv.COLOR_HSV2BGR)
 
-desired_width = 800  
-desired_height = 600  
+    return shifted_image
 
+def get_hue(image):
+    hsv_image = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+    hue_values = hsv_image[:, :, 0]
+    return hue_values
+
+input_image = cv.imread('images/random_image_ (8).jpg')
+resized_input = cv.resize(input_image, (800, 600), interpolation=cv.INTER_LINEAR)
+
+desired_width = 800
+desired_height = 600
 resized_image = cv.resize(input_image, (desired_width, desired_height), interpolation=cv.INTER_LINEAR)
 
-cv.imwrite('resized_image.jpg', resized_image)
+target_hue = 90  
 
+result_image = shift_hue(resized_image, target_hue)
+hue_values = get_hue(resized_image)
+for row in hue_values:
+    print(row) 
 
-w, h = (128,128)
-
-temp = cv.resize(resized_image, (w, h), interpolation=cv.INTER_LINEAR)
-
-output = cv.resize(temp, (800, 600), interpolation=cv.INTER_NEAREST)
-
-cv.imshow('Input', resized_image)
-cv.imshow('Output', output)
-
+cv.imwrite('shifted_image.jpg', result_image)
+cv.imshow('Shifted Image', result_image)
+cv.imshow("Original",resized_input)
 cv.waitKey(0)
+cv.destroyAllWindows()
